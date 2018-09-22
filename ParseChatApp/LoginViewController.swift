1f90c484072d48280ab3c3203896db0991c7f00e
paul <paul@pauls-MacBook-Pro.local>
2018-09-22 00:32:31 -0700

    login alert setup, milestone2 complete

---
+++ ParseChatApp/LoginViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  ParseChatApp
+//
+//  Created by paul on 9/21/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameInput: UITextField!
+    
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func signUpBtn(_ sender: Any) {
+        if (usernameInput.text?.isEmpty)! || (passwordInput.text?.isEmpty)! {
+            loginViewAlert()
+            
+        }else{
+            
+            registerUser()
+        }
+        
+    }
+    
+    
+    @IBAction func loginBtn(_ sender: Any) {
+        if (usernameInput.text?.isEmpty)! || (passwordInput.text?.isEmpty)! {
+            loginViewAlert()
+            
+        }else{
+            
+            loginUser()
+        }
+        
+    }
+    
+    
+    
+    func registerUser() {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameInput.text
+        //newUser.email = emailField.text
+        newUser.password = passwordInput.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    func loginUser() {
+        
+        let username = usernameInput.text ?? ""
+        let password = passwordInput.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    
+    
+    
+    func loginViewAlert(){
+        
+        let title = "Error"
+        let message: String
+        if (usernameInput.text?.isEmpty)! && (passwordInput.text?.isEmpty)!{
+            message = "Please Enter Username and Password."
+        }else if (usernameInput.text?.isEmpty)!{
+            message = "Please Enter Username."
+        }else{
+            message = "Please Enter Password."
+        }
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    
+
+}
+
